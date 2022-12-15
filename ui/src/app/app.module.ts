3d34a2035f7054df769e72a90a6a0df91d4e7ab6
Leonardo Guerra <leokaos@gmail.com>
2022-12-15 01:49:10 +0000

    vamos de novo

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -4,18 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import {CardModule} from 'primeng/card';
-import {ButtonModule} from 'primeng/button';
-
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    CardModule,
-    ButtonModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
