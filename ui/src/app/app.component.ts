b70c63b3af82664f656134230ca431136a3464d2
Leonardo Guerra <leokaos@gmail.com>
2022-12-16 14:14:54 +0000

    criei um logotipo

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AppConstants } from './app.constants';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ui';
+export class AppComponent implements OnInit {
+
+  private title?: String;
+
+  constructor(private constants: AppConstants) { }
+
+  ngOnInit(): void {
+    this.title = this.constants.TITLE;
+  }
+
 }
---
+++ ui/src/app/app.constants.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class AppConstants {
+    public TITLE = "Wise Test";
+}
