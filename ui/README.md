10859dc5ecc73c1174f62426a62ed024d91bd536
Leonardo Guerra <leokaos@gmail.com>
2023-05-05 21:34:47 +0100

    vish

--- ui/README.md
+++ ui/README.md
@@ -1,6 +1,6 @@
-# Ui
+# Cliente
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 15.0.3.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 15.1.5.
 
 ## Development server
 
--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -4,6 +4,18 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarModule } from 'primeng/sidebar';
+import { ButtonModule } from 'primeng/button';
+import { ToolbarModule } from 'primeng/toolbar';
+import { CardModule } from 'primeng/card';
+import { TabMenuModule } from 'primeng/tabmenu';
+import { TabViewModule } from 'primeng/tabview';
+import { MenubarModule } from 'primeng/menubar';
+import { AvatarModule } from 'primeng/avatar';
+import { BadgeModule } from 'primeng/badge';
+import { DropdownModule } from 'primeng/dropdown';
+import { FormsModule } from '@angular/forms';
+import { PanelMenuModule } from 'primeng/panelmenu';
 
 @NgModule({
   declarations: [
@@ -12,7 +24,19 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    SidebarModule,
+    ButtonModule,
+    ToolbarModule,
+    CardModule,
+    TabMenuModule,
+    TabViewModule,
+    MenubarModule,
+    AvatarModule,
+    BadgeModule,
+    DropdownModule,
+    PanelMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
